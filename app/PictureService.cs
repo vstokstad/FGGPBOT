3b34001584f01754d0dbdbde4a445eb08abffe27
Vilhelm Stokstad <v.stokstad@gmail.com>
2021-01-04 11:20:16 +0100

    updated cat with THor fallback

--- app/PictureService.cs
+++ app/PictureService.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace fggpbot {
+namespace app {
     public class PictureService {
         private readonly HttpClient _http;
 
